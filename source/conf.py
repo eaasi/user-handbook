bcc3bb6e39b7953ff5a52188988d5de795e6e50a
EG-tech <ethan.gates@yale.edu>
2020-04-08 15:40:04 -0400

    fix page width
    add custom CSS to optimize for desktops rather than mobile

--- source/conf.py
+++ source/conf.py
@@ -79,6 +79,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files= ['custom.css']
+
 html_logo = "_static/logo.svg"
 
 # Custom sidebar templates, must be a dictionary that maps document names
